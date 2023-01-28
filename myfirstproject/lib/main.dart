c4ee55eb224e4740b7787e2425fd693cd80c68a6
Abhay <abhayshukralia3003@gmail.com>
2023-01-29 00:29:40 +0530

    my-first-commit

---
+++ myfirstproject/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Screen"),
+        ),
+        body: Center(
+          child: Text("Welcome to the home screen!"),
+        ),
+      ),
+    );
+  }
+}
