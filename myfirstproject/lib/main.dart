42de3e9633c53cbd395aa7c234a421c9c9907cbb
Abhay <abhayshukralia3003@gmail.com>
2023-01-29 14:55:16 +0530

    day4 completed

--- myfirstproject/lib/main.dart
+++ myfirstproject/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:myfirstproject/pages/HomePage.dart';
 import 'package:myfirstproject/pages/loginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
-
-    themeMode: ThemeMode.dark,
-    theme: ThemeData(primarySwatch: Colors.deepPurple),
-    darkTheme: ThemeData(
-      primarySwatch: Colors.deepPurple),
-      initialRoute: "/",
-     routes: {
-      "/":(context)=>HomePage(),
-      "/login":(context)=>HomePage(),
-      "/login":(context) => loginPage()
-     },
+    themeMode: ThemeMode.light,
+    theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        scaffoldBackgroundColor: Colors.blue),
+        
+    darkTheme: ThemeData(primarySwatch: Colors.deepPurple),
+    initialRoute: "/login",
+    routes: {
+      "/": (context) => HomePage(),
+      "/login": (context) => HomePage(),
+      "/login": (context) => loginPage()
+    },
   ));
 }
- 
--- myfirstproject/lib/pages/loginPage.dart
+++ myfirstproject/lib/pages/loginPage.dart
@@ -11,18 +11,53 @@ class loginPage extends StatelessWidget {
   // void bringvegetables({@required int bucket,int ruppes =10}) if ruppes not givern initilse it eith 10
 
   Widget build(BuildContext context) {
-    int days = 5;
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Login Page",style: TextStyle(
-            fontSize: 30,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold
-          ),),
+    // int days = 5;
+    return Material(
+      
+        child: Column(
+      children: [
+        Image.asset(
+          "assets/images/loginImage.png",
+          fit: BoxFit.cover,
         ),
-      ),
-    );
-    ;
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Welcome",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child: Column(
+            children: [
+              TextField(
+                  decoration: InputDecoration(
+                      hintText: "Enter UserName", labelText: "UserName")),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Password", labelText: "Password"),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ElevatedButton(
+          child: Text("Login"),
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.deepPurpleAccent, // Background Color
+          ),
+          onPressed: () {
+            print("Wowwww! My app is working ");
+          },
+        )
+      ],
+    ));
   }
 }
